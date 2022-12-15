d1cc9b46afe63a7851d149ca15b61a941ef6fcef
Andrea-Baggio <andreabaggio800@gmail.com>
2022-12-15 16:30:52 +0100

    problema con forEach

--- index.php
+++ index.php
@@ -5,8 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>CIAO</h1>
+    <?php 
+    $arrFaq = [
+        $firstPart = [
+            'question' => 'Come stai?',
+        ],
+
+        $answer1 = [
+            'paragraph1' => 'Tutto bene, grazie.',
+            'paragraph2' => 'Te come stai?',
+        ],
+
+        $secondPart = [
+            'question' => 'Nevica?',
+        ],
+    
+        $answer2 = [
+            'paragraph1' => 'No, piove.',
+        ],
+    ];
+    ?>
+
+    <div class="container">
+    <?php
+        foreach ($firstPart as $value) {
+                echo " $value" . '<br>';     
+            foreach ($answer1 as $value) {
+                echo " $value" . '<br>';                
+            }
+        }
+    ?>
+    </div>
 </body>
 </html>
